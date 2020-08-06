6a0fd97a9ce2b7de4bbf8d17cb5b6d4ef718906b
Jake <jakepowis@gmail.com>
2020-08-06 14:13:24 +0100

    error handling

--- server/index.js
+++ server/index.js
@@ -14,6 +14,8 @@ connectDB();
 app.use(cors())
 app.use(express.json());
 
+app.use(favicon(__dirname + '/public/favicon.ico'));
+
 // Define Routes
 app.use('/', require('./routes/index'));
 app.use('/api/url', require('./routes/url'));
@@ -23,9 +25,6 @@ app.use('/api/url', require('./routes/url'));
 if (process.env.NODE_ENV === 'production') {
     // Set static folder
     app.use(express.static('../client/build'));
-
-    app.use(favicon(__dirname + '/public/favicon.ico'));
-
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
